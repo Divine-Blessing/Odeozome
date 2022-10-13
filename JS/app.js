89dc7ef669689b5a26e3ede6b429b03f6ef7e71f
odeozome <78852587+Divine-Blessing@users.noreply.github.com>
2022-10-13 19:18:03 +0100

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+// MENU BAR
 const menuBar = document.querySelector('.menu');
 const sideBar = document.querySelector('.links');
 menuBar.addEventListener('click', () => {
@@ -14,3 +15,32 @@ menuBar.addEventListener('click', () => {
 })
 
 
+// FADE-IN EFFECT
+AOS.init();
+
+// FADE-IN EFFECT IN RESUME.HTML
+// const faders = document.querySelectorAll('.fade-in');
+
+// sectionOneObserver.observe(sectionOne);
+// const appearOptions = {
+//     threshold:  1,
+//     rootMargin: "0px 0px -100px 0px"
+// };
+// const appearOnScroll =  new IntersectionObserver
+// (function(
+//     entries, appearOnScroll
+// ) {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting){
+//             return;
+//         } else{
+//             entry.target.classList('appear');
+//             appearOnScroll.unobserve(entry.target);
+//         }
+//     })
+// },
+// appearOptions);
+
+// faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+// })
